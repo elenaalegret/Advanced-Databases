565b09f7629308e58b29f77d77a801522aea2b93
Elena Alegret Regalado <105710270+elenaalegret@users.noreply.github.com>
2024-03-19 17:26:46 +0100

    Add files via upload

---
+++ data_collectors/criminal_dataset.py
@@ -0,0 +1,21 @@
+#### Dataset Preparation
+
+# Imports
+import pandas as pd
+import requests
+import io
+
+# Datasets URL
+dataset_urls = {
+    'criminal_acts': 'https://analisi.transparenciacatalunya.cat/resource/y48r-ae59.json?$limit=50000000'
+}
+
+# Retrieving and Reading Criminal Dataset
+criminal_dataset = requests.get(dataset_urls['criminal_acts']).content
+criminal_dataset = pd.read_json(io.StringIO(criminal_dataset.decode('utf-8')))
+
+# Load and read in .parquet format
+criminal_dataset.to_parquet('criminal_dataset.parquet')
+#criminal_dataset = pd.read_parquet('criminal_dataset.parquet')
+
+
