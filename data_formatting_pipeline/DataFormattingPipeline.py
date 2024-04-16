2bd3399d62e4a0b2bd0c0e32c4b828c54d2b458d
Sergi Tomàs <sergi.tomas.martinez@estudiantat.upc.edu>
2024-04-16 19:43:59 +0200

    wwowowowo

--- data_formatting_pipeline/DataFormattingPipeline.py
+++ data_formatting_pipeline/DataFormattingPipeline.py
@@ -9,33 +9,64 @@ import duckdb
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, upper, when, length, lit
 
-spark = SparkSession.builder.appName("DataFormattingPipeline").getOrCreate()
+
+
+con = duckdb.connect(database='./../data/formatted_zone/barcelona.db', read_only=False)
+con.close()
+
+print('Iniciant Spark')
+spark = SparkSession.builder\
+    .config("spark.jars", "duckdb_jdbc-0.10.1.jar") \
+    .getOrCreate()
+
 
 ## Prepare the dataset for DuckDB framework
 
+print('Iniciant Parquets')
+
 # Load .parquet database to Pandas Dataframe 
-df_airbnb_listings = spark.read.parquet('../data/landing_zone/airbnb_listings.parquet')
-df_criminal_dataset = spark.read.parquet('../data/landing_zone/criminal_dataset.parquet')
-df_tripadvisor_locations = spark.read.parquet('../data/landing_zone/tripadvisor_locations.parquet')
-df_tripadvisor_reviews = spark.read.parquet('../data/landing_zone/tripadvisor_reviews.parquet')
+df_airbnb_listings = spark.read.parquet('./../data/landing_zone/airbnb_listings.parquet')
+df_criminal_dataset = spark.read.parquet('./../data/landing_zone/criminal_dataset.parquet')
+df_tripadvisor_locations = spark.read.parquet('./../data/landing_zone/tripadvisor_locations.parquet')
+df_tripadvisor_reviews = spark.read.parquet('./../data/landing_zone/tripadvisor_reviews.parquet')
 
-# Make connection to DuckDB database
-con = duckdb.connect(database='barcelona', read_only=False)
 
-# Insert DataFrame to DuckDB
-con.register('airbnb_listings', df_airbnb_listings)
-con.register('criminal_dataset', df_criminal_dataset)
-con.register('tripadvisor_locations', df_tripadvisor_locations)
-con.register('tripadvisor_reviews', df_tripadvisor_reviews)
+
+
+
+print('Escrivint 1')
+
+jdbc_url = 'jdbc:duckdb:./../data/formatted_zone/barcelona.db'
+driver = "org.duckdb.DuckDBDriver"
+
+df_airbnb_listings.write \
+    .format("jdbc") \
+    .option("url", jdbc_url) \
+    .option("dbtable", "df_airbnb_listings") \
+    .option("driver", driver) \
+    .save()
+
+df_criminal_dataset.write \
+    .format("jdbc") \
+    .option("url", jdbc_url) \
+    .option("dbtable", "df_criminal_dataset") \
+    .option("driver", driver) \
+    .save()
+
+df_tripadvisor_locations.write \
+    .format("jdbc") \
+    .option("url", jdbc_url) \
+    .option("dbtable", "df_tripadvisor_locations") \
+    .option("driver", driver) \
+    .save()
+
+df_tripadvisor_reviews.write \
+    .format("jdbc") \
+    .option("url", jdbc_url) \
+    .option("dbtable", "df_tripadvisor_reviews") \
+    .option("driver", driver) \
+    .save()
 
 spark.stop()
 
 
-"""
-# CONSULT EXAMPLE:
-query_airbnb_listings = "SELECT * FROM airbnb_listings;"
-
-# Execute consult SQL and take results 
-result_airbnb_listings = con.execute(query_airbnb_listings).fetchall()
-print(result_airbnb_listings)
-"""
