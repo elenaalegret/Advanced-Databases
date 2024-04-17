6f5215c8cfe4755a1df5c177a6bbc6310fec4980
elenaalegret <elenaalegret@gmail.com>
2024-04-17 08:40:52 +0200

    DataFormatting done

--- data_formatting_pipeline/DataFormattingPipeline.py
+++ data_formatting_pipeline/DataFormattingPipeline.py
@@ -1,44 +1,53 @@
 ###                           ###
-### Data Formatting Pipeline ###
+### Data Formatting Pipeline  ###
 ###                           ###
 
-
 # Import
-import pandas as pd
+import os
 import duckdb
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, upper, when, length, lit
+from pyspark.sql.functions import col, array_join, concat_ws
 
 
 
+## Prepare the DuckDB connection
 con = duckdb.connect(database='./../data/formatted_zone/barcelona.db', read_only=False)
 con.close()
 
-print('Iniciant Spark')
+## Spark
+print('Spark Inicialitzation...')
 spark = SparkSession.builder\
     .config("spark.jars", "duckdb_jdbc-0.10.1.jar") \
+    .appName("DataFormatting") \
     .getOrCreate()
 
 
-## Prepare the dataset for DuckDB framework
-
-print('Iniciant Parquets')
-
-# Load .parquet database to Pandas Dataframe 
+## Load .parquet database with spark 
+print('Reading .parquet files...')
 df_airbnb_listings = spark.read.parquet('./../data/landing_zone/airbnb_listings.parquet')
 df_criminal_dataset = spark.read.parquet('./../data/landing_zone/criminal_dataset.parquet')
 df_tripadvisor_locations = spark.read.parquet('./../data/landing_zone/tripadvisor_locations.parquet')
 df_tripadvisor_reviews = spark.read.parquet('./../data/landing_zone/tripadvisor_reviews.parquet')
 
+### As the Airbnb dataset gives us problems, we need it to make a preparatory preprocessing
+df_airbnb_listings.printSchema()
+
+#### Convert array type columns to strings separated by commas and remove the ones that can not be preprocess
+df_airbnb_listings = df_airbnb_listings.withColumn("host_verifications", concat_ws(", ", col("host_verifications")))
+df_airbnb_listings = df_airbnb_listings.withColumn("amenities", concat_ws(", ", col("amenities")))
+df_airbnb_listings = df_airbnb_listings.withColumn("features", concat_ws(", ", col("features")))
+
+#### Save DataFrame and read it again
+df_airbnb_listings.write.mode("overwrite").parquet("./../data/landing_zone/airbnb_listings2.parquet")
+df_airbnb_listings = spark.read.parquet('./../data/landing_zone/airbnb_listings2.parquet')
 
 
-
-
-print('Escrivint 1')
-
+## Write on the tables 
+print('Writting tables...')
 jdbc_url = 'jdbc:duckdb:./../data/formatted_zone/barcelona.db'
 driver = "org.duckdb.DuckDBDriver"
 
+print('    - Aribnb table')
 df_airbnb_listings.write \
     .format("jdbc") \
     .option("url", jdbc_url) \
@@ -46,6 +55,7 @@ df_airbnb_listings.write \
     .option("driver", driver) \
     .save()
 
+print('    - Criminal table')
 df_criminal_dataset.write \
     .format("jdbc") \
     .option("url", jdbc_url) \
@@ -53,6 +63,7 @@ df_criminal_dataset.write \
     .option("driver", driver) \
     .save()
 
+print('    - Tripadvisor Locations table')
 df_tripadvisor_locations.write \
     .format("jdbc") \
     .option("url", jdbc_url) \
@@ -60,6 +71,7 @@ df_tripadvisor_locations.write \
     .option("driver", driver) \
     .save()
 
+print('    - Tripadvisor Reviews table')
 df_tripadvisor_reviews.write \
     .format("jdbc") \
     .option("url", jdbc_url) \
