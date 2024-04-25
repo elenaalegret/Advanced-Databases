8105f1e74c254d0882b39083c941233020211bf0
Julia Orteu <juliaorteu@Julias-Laptop.local>
2024-04-25 15:45:25 +0200

    Arreglos de visualitzacio

--- data_preparation_pipeline/app.py
+++ data_preparation_pipeline/app.py
@@ -34,9 +34,10 @@ st.write("Welcome! Choose your neighborhood 🏘️ and explore local restaurant
 # Additional resources would enable processing of larger datasets.
 # --> Constarint less apartments for visualization in local:  num_samples/1000 -- only 10% of the total data
 # --> If you have resources descoment the indicated line
-num_samples = st.sidebar.slider("Percentage of Airbnb Locations", min_value=1, max_value=100, value=1) # Sample the Airbnb dataset
+num_samples = st.sidebar.slider("Percentage of Locations Displayed", min_value=1, max_value=100, value=1)
 #sampled_data = df_airbnb.sample(withReplacement=False, fraction=num_samples/100, seed=42) # MORE RESOURCES
 sampled_data = df_airbnb.sample(withReplacement=False, fraction=num_samples/1000, seed=42) # LESS RESOURCES
+sampled_locations = df_locations.sample(withReplacement=False, fraction=num_samples/100, seed=42) 
 #__________________________________________________________________________________________________________
 
 selected_neighborhoods = {}
@@ -48,7 +49,7 @@ with st.sidebar.expander("Neighborhoods Color Legend"):
         st.markdown(f"<span style='display: inline-block; width: 12px; height: 12px; background: {color};'></span>", unsafe_allow_html=True)
 
 filtered_data = sampled_data[sampled_data['neighbourhood'].isin([neighborhood for neighborhood, selected in selected_neighborhoods.items() if selected])]
-filtered_locations = df_locations[df_locations['neighbourhood'].isin([neighborhood for neighborhood, selected in selected_neighborhoods.items() if selected])]
+filtered_locations = sampled_locations[sampled_locations['neighbourhood'].isin([neighborhood for neighborhood, selected in selected_neighborhoods.items() if selected])]
 
 # Creation of a map visualization of Barcelona
 m = folium.Map(location=[41.3879, 2.1699], zoom_start=12)
@@ -74,7 +75,39 @@ for row in filtered_locations.collect():
         icon=folium.Icon(color=colors.get(row['neighbourhood'], 'gray'), icon=location_icons.get(row['type']))
     ).add_to(m)
 
-# Mostrar el mapa
+# Display the number of apartments & Locations
+st.markdown(f'''
+<div style="
+    border-radius: 10px;
+    border: 2px solid #ff9832;
+    padding: 15px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    font-size: 16px;
+    color: #ff9832;
+    background-color: #ffffff;
+    box-shadow: 2px 2px 12px rgba(0,0,0,0.1);">
+    <b> Displayed Apartments </b> {filtered_data.count()}
+</div>
+''', unsafe_allow_html=True)
+
+st.markdown(f'''
+<div style="
+    border-radius: 10px;
+    border: 2px solid #a26464;
+    padding: 15px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    font-size: 16px;
+    color: #a26464;
+    background-color: #ffffff;
+    box-shadow: 2px 2px 12px rgba(0,0,0,0.1);">
+    <b>Displayed Restaurants:</b> {filtered_locations.filter(filtered_locations['type'] == 'restaurant').count()}<br>
+    <b>Displayed Attractions:</b> {filtered_locations.filter(filtered_locations['type'] == 'attraction').count()}
+</div>
+''', unsafe_allow_html=True)
+
+# Show the map
 folium_static(m)
 
 
